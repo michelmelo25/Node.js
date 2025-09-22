fb2e83004d740c9a94e4d1192a209d98286b8895
Michel <michelmelosj25@hotmail.com>
2025-09-22 17:38:32 -0300

    Estrutura da aplicacao http

--- Fundamentos-NodeJS/anotacoes.md
+++ Fundamentos-NodeJS/anotacoes.md
@@ -1,2 +1,12 @@
+- INIANDO COM NODE.JS E ESTRUTURA DA APLICAÇÃO
+
 importações interna do node.js deve conter o prefixo 'node:'
 ex: import http from 'node:http' 
+
+// iniciar/criar projeto noje
+npm init -y
+
+
+Statfull sempre tera informções sava em memoria
+
+Stateless não salva nada em memoria, sempre dispositivo esterno como banco de dados ou arquivo
--- Fundamentos-NodeJS/src/server.js
+++ Fundamentos-NodeJS/src/server.js
@@ -1,7 +1,52 @@
 import http from 'node:http' 
 
+// - Criar usuário
+// - Listagem de Usuários
+// - Edição de Usuários
+// - Remoção de Usuários
+
+// - HTTP
+//    - Método HTTP
+//    - URL
+//    - Cabeçalhos
+//    - Corpo (body)
+
+// GET => Busca um recurso do back-end
+// POST => Cria um recurso no back-end
+// PUT => Atualiza um recurso do back-end
+// PATCH => Atualiza uma informação específica de um recurso no back-end
+// DELETE => Remove um recurso do back-end
+
+// GET /users => Busca um recurso do Back-end
+// POST /users => Cria um recurso no Back-end
+
+// Stateful - Stateless
+
+// Cabeçalhos (Requisição/resposta) => Metadados
+
+// HTTP Status Code
+
+const users = []
+
 const server = http.createServer((request, response) => {
-  return response.end('Hello World')
+  const {method, url} = request
+
+  if(method === 'GET' && url === '/users'){
+    return response
+    .setHeader('Content-Type', 'application/json')
+    .end(JSON.stringify(users))
+  }
+
+
+  if(method === 'POST' && url ==='/users'){
+    users.push({
+      id:1,
+      name: 'John Doe',
+      email: 'johndoe@example.com'})
+
+      return response.writeHead(201).end()
+  }
+  return response.writeHead(404).end()
 })
 
 server.listen(3333) 
