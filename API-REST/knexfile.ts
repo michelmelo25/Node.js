6d274f0dbb095a5578a19878257a22901ae11c91
Michel <michelmelosj25@hotmail.com>
2025-09-29 16:56:59 -0300

    refatorando estrutura, knex com erro na importacao

--- API-REST/knexfile.ts
+++ API-REST/knexfile.ts
@@ -1,3 +1,15 @@
-import { config } from './src/database.js'
+import { Knex } from 'knex'
+
+const config: Knex.Config = {
+  client: 'sqlite',
+  connection: {
+    filename: './db/app.db',
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
 
 export default config
--- API-REST/src/database.ts
+++ API-REST/src/database.ts
@@ -1,15 +1,16 @@
-import { knex as setupKnex, Knex } from 'knex'
+import { knex as setupKnex } from 'knex'
+import config from '../knexfile.js'
 
-export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: './db/app.db',
-  },
-  useNullAsDefault: true,
-  migrations: {
-    extension: 'ts',
-    directory: './db/migrations',
-  },
-}
+// export const config: Knex.Config = {
+//   client: 'sqlite',
+//   connection: {
+//     filename: './db/app.db',
+//   },
+//   useNullAsDefault: true,
+//   migrations: {
+//     extension: 'ts',
+//     directory: './db/migrations',
+//   },
+// }
 
 export const knex = setupKnex(config)
