3424013f3e9d63ef57b337f1e758867e99bbc36b
Michel <michelmelosj25@hotmail.com>
2025-10-14 09:32:15 -0300

    variavel ambiente com validação via zod

---
+++ API-REST/src/env/index.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(['develompment', 'test', 'production'])
+    .default('production'),
+  DATABASE_URL: z.string(),
+  PORT: z.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error(
+    'Invalide environment variables!\n',
+    z.prettifyError(_env.error),
+  )
+  //   console.error(z.treeifyError(_env.error))
+
+  throw new Error('Invalide environment variables!')
+}
+
+export const env = _env.data
--- API-REST/src/server.ts
+++ API-REST/src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -11,19 +12,20 @@ app.get('/hello', async () => {
 })
 
 app.post('/hello', async () => {
-  const transaction = await knex('transactions').insert({
-    id: crypto.randomUUID(),
-    title: 'Tansação de teste0',
-    amount: 1000,
-  }).returning('*')
-  
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Tansação de teste0',
+      amount: 1000,
+    })
+    .returning('*')
+
   return transaction
 })
 
-
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server running on http://localhost:3333')
