0b4f46cabe9045213cba942c257da00e41d692cf
Michel <michelmelosj25@hotmail.com>
2025-10-14 12:25:59 -0300

    listando as trasações e resumo da movimentação

--- API-REST/src/routes/transactions.ts
+++ API-REST/src/routes/transactions.ts
@@ -4,6 +4,32 @@ import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
 
 export async function transactionsRotes(app: FastifyInstance) {
+  app.get('/', async (request, replay) => {
+    const transactions = await knex('transactions').select('*')
+
+    return replay.status(200).send({ transactions })
+  })
+
+  app.get('/:id', async (request, replay) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return replay.status(200).send({ transaction })
+  })
+
+  app.get('/summary', async (request, replay) => {
+    const transactions = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return replay.status(200).send({ transactions })
+  })
+
   app.post('/', async (request, replay) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
